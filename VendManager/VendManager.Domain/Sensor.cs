c450781b518dfc159197864e512ccbfc0bb06c14
Simuzeche Kaluwa <simuzechek@gmail.com>
2024-07-11 19:19:12 +0200

    Feat : added Sensor Entity

---
+++ VendManager/VendManager.Domain/Sensor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendManager.Domain
+{
+    public class Sensor
+    {
+        public int ID { get; set; }
+        public int SensorBarID { get; set; }
+        public int ThresholdValue { get; set; }
+        public int CurrentValue { get; set; }
+        public DateTime LastUpdatedDateTime { get; set; }
+        public int ChuteNumber { get; set; }
+        public bool Enabled { get; set; }
+
+    }
+}
