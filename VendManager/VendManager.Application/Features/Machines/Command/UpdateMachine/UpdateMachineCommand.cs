7225dc654ecf75be63db6ecd3346675bd8701a08
Simuzeche Kaluwa <simuzechek@gmail.com>
2024-07-14 17:41:02 +0200

    Feat : Added UpdateMachine Vertical Slice functionality

---
+++ VendManager/VendManager.Application/Features/Machines/Command/UpdateMachine/UpdateMachineCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendManager.Application.Features.Machines.Command.UpdateMachine
+{
+    public class UpdateMachineCommand : IRequest<Unit>
+    {
+        public string Name { get; set; }
+        public string SerialNumber { get; set; }
+        public string Location { get; set; }
+        public string Notes { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
---
+++ VendManager/VendManager.Application/Features/Machines/Command/UpdateMachine/UpdateMachineCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VendManager.Application.Persistence;
+
+namespace VendManager.Application.Features.Machines.Command.UpdateMachine
+{
+    public class UpdateMachineCommandHandler : IRequestHandler<UpdateMachineCommand, Unit>
+    {
+
+        private readonly IMapper _mapper;
+        private readonly IMachinesRepository _machineRepository;
+
+        public UpdateMachineCommandHandler(IMapper mapper, IMachinesRepository machineRepository)
+        {
+            _mapper = mapper;
+            _machineRepository = machineRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateMachineCommand request, CancellationToken cancellationToken)
+        {
+            //Validate Incoming Data
+
+
+            //Convert to Domain Entities
+            var machine = _mapper.Map<Domain.Machines>(request);
+
+            //Save to Database
+            await _machineRepository.UpdateAsync(machine);
+
+            //Return
+            return Unit.Value;
+        }
+    }
+    
+}
