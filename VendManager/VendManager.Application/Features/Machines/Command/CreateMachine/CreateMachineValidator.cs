2611162cc2bc4cf5d22aa2b21008c368d7300f1f
Simuzeche Kaluwa <simuzechek@gmail.com>
2024-07-14 17:57:47 +0200

    Feat : added CreateMachineValidator

---
+++ VendManager/VendManager.Application/Features/Machines/Command/CreateMachine/CreateMachineValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendManager.Application.Features.Machines.Command.CreateMachine
+{
+    public class CreateMachineValidator : AbstractValidator<CreateMachineCommand>
+    {
+        public CreateMachineValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
+
+            RuleFor(p => p.SerialNumber)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
+
+            RuleFor(p => p.Location)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
+
+            RuleFor(p => p.Notes)
+                .MaximumLength(500).WithMessage("{PropertyName} must not exceed 500 characters.");
+
+     
+        }
+    }
+}
