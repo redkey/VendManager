7125e96246a0368b749e7f8668bde28e88ef51d2
Simuzeche Kaluwa <simuzechek@gmail.com>
2024-07-14 15:55:43 +0200

    Feat : added GetMachineQuery

---
+++ VendManager/VendManager.Application/Features/Machines/Query/GetAllMachines/GetMachineQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendManager.Application.Features.Machines.Query.GetAllMachines
+{
+    public record GetMachineQuery : IRequest<List<MachineDto>>;
+    
+}
