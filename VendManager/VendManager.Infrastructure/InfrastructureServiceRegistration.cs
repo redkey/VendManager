7d7232a4efb681d352b984a879d8500b4b35f2aa
Simuzeche Kaluwa <simuzechek@gmail.com>
2024-07-15 14:22:19 +0200

    Feat : Added API project

---
+++ VendManager/VendManager.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace VendManager.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
+    
